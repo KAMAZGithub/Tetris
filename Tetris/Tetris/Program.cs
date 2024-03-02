878180b81dbadcb4e0fece273744f8b2082fc732
KAMAZGithub <saprcon@yandex.ru>
2024-03-02 14:52:00 +0300

    Перемещение курсора в консоли

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -6,11 +6,31 @@ using System.Threading.Tasks;
 
 namespace Tetris
 {
-   class Program
-   {
-      static void Main(string[] args)
-      {
-         Console.WriteLine("Hello!!!");
-      }
-   }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            int x1 = 2;
+            int y1 = 3;
+            char ch1 = '*';
+
+            Draw(x1, y1, ch1);
+
+            int x2 = 5;
+            int y2 = 4;
+            char ch2 = '#';
+
+            Draw(x2, y2, ch2);
+
+        }
+
+        private static void Draw(int x, int y, char ch)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(ch);
+        }
+    }
 }
