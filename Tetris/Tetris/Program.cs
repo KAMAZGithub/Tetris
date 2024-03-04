f4b4a0eb5b03c7ac1fdd0506f686c708e2bd0744
KAMAZGithub <saprcon@yandex.ru>
2024-03-04 22:19:53 +0300

    Добавили класс Square

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -13,16 +13,19 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            Point p1 = new Point(1, 1, '*');
-            p1.Draw();
+            //Point p1 = new Point(1, 1, '*');
+            //p1.Draw();
 
-            Point p2 = new Point()
-            {
-                x = 2,
-                y = 2,
-                ch = '*'
-            };
-            p2.Draw();
+            //Point p2 = new Point()
+            //{
+            //    x = 2,
+            //    y = 2,
+            //    ch = '*'
+            //};
+            //p2.Draw();
+
+            Square s = new Square(2, 5, '*');
+            s.Draw();
 
         }
 
---
+++ Tetris/Tetris/Square.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Square
+    {
+        Point[] points = new Point[4];
+
+        public Square(int x, int y, char ch)
+        {
+            points[0] = new Point(x, y, ch);
+            points[1] = new Point(x + 1, y, ch);
+            points[2] = new Point(x, y + 1, ch);
+            points[3] = new Point(x + 1, y + 1, ch);
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in points)
+                p.Draw();
+        }
+    }
+}
